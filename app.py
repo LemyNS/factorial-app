3ffc0fd0fb2bd97cf16f551b0f5f3ab6d27d1890
LemyNS <myhtlee@gmail.com>
2025-06-16 15:11:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 from factorial import fact
 
 def main():
-  st.title("Tinh giai thua")
-  number = st.numner_inpurt("Tinh giai thua:", min_value=0, max_value = 900)
-  if st.button("Calculate"):
-    result = fact(number)
-    st.write(f"Giai thua cua {number} la {result}")
-  
+    st.title("Tính Giai Thừa")
+
+    number = st.number_input("Nhập số để tính giai thừa:", min_value=0, max_value=900, step=1)
+
+    if st.button("Tính toán"):
+        result = fact(number)
+        st.write(f"Giai thừa của {number} là {result}")
+
 if __name__ == "__main__":
     main()
